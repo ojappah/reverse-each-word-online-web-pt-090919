5b8d88908ab53a545e8ba07127796272468b2150
generous-vector-9581 <ojappah@gmail.com>
2019-09-21 00:10:54 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 sentence = ["Hello there, and how are you?"]
 def reverse_each_word(sentence)
 new_sentence =[]
-sentence.each {|words| new_sentence.reverse << reverse_each_word(sentence)}
+sentence.each {|words.reverse| new_sentence << reverse_each_word(sentence)}
 return new_sentence
 end
 
