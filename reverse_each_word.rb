fcff194d4ffb156864cfec263077de443fcf9d09
generous-vector-9581 <ojappah@gmail.com>
2019-09-21 01:21:19 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -11,3 +11,16 @@ def reverse_each_word(sentence)
  end
  n.strip
 end
+def reverse_each_word(sentence)
+  n = ""
+  sentence.split(' ').collect do |w|
+    c = w.length
+    nc = ''
+    while(c >= 1) do
+      c-=1
+      nc+=w[c]
+    end
+    n+= " #{nc}"
+ end
+ n.strip
+end
